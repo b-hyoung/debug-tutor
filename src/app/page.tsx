4eb3de45d72e5f75237e57196d4b0b171257285a
b-hyoung <youqlrqod@gmail.com>
2025-09-04 15:59:33 +0900

    add init page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
 
+type MyData = {
+  id: number;
+  name: string;
+  active: boolean;
+};
+
+
 export default function Home() {
+
+  const abd: MyData = {
+    id: 1,
+    name: "test",
+    active: true,
+};
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,7 +30,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
+              {abd.id}
             </code>
             .
           </li>
